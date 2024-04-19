277e7ea65df033dd3239a38173159f7e5e8d4cd9
Eduardo Marquis <eduardomarquis8@gmail.com>
2024-04-19 17:30:57 -0400

    ALMOSSO

--- cypress/e2e/pedirallmoso.test.cy.js
+++ cypress/e2e/pedirallmoso.test.cy.js
@@ -1,13 +1,67 @@
+import acesso from '../../cypress.env.json'
+
 describe("acessa o google", () => {
   it("search", () => {
-    cy.visit('https://www.agendaassessoria.com.br/')
-    cy.get(':nth-child(2) > .btn-nav-line').click()
-    cy.visit('https://www.agendaextranet.com.br/NovaExtranet/Login.aspx')
-    cy.get('id="txtID"').type('2730')
-    cy.get('id="txtSenha"').type('Eduardo2007')
+   
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      // Verifica se a mensagem de erro contém "Bootstrap's JavaScript requires jQuery"
+      if (err.message.includes("Bootstrap's JavaScript requires jQuery")) {
+        // Retorna false para ignorar o erro
+        return false
+      }
+      // Retorna true para capturar e tratar outros erros
+      return true
+    })
+    
+    cy.visit('https://www.agendaextranet.com.br/NovaExtranet/ConfirmarCardapio.aspx') 
+    
+   
+    cy.get('[id="txtID"]').type(acesso.usuario, {log :false})
+    cy.get('[id="txtSenha"]').type(acesso.senha, {log:false}) 
+    cy.get('[id="lnkLogin"]').click()
+    
+
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      // Verifica se a mensagem de erro contém "a.widget is not a function"
+      if (err.message.includes("a.widget is not a function")) {
+        // Retorna false para ignorar o erro
+        return false;
+      }
+      // Retorna true para capturar e tratar outros erros
+      return true;
+    });
+
+   cy.visit('https://www.agendaextranet.com.br/NovaExtranet/ConfirmarCardapio.aspx')
+
+
+
+   //cy.get('ul.list-group').should(($element) => {
+    //if ($element.text() === 'class="list-group-item') {
+      // Execute uma ação se a condição for verdadeira
+   //cy.get('id="rdComplemento_1"').click()
+    //} else {
+      // Execute uma ação se a condição for falsa
+      //cy.get('.outro-seletor-alternativo').click();
+   // }
+  //});
 
 
 
 
+
+
+
+
+
+
+
+
+
+   cy.get('[id="btnConfirmar"]').click()
+    //cy.get('ul#ulMenuTopo .glyphicon.glyphicon-cutlery').click()
+
+
+    //cy.visit('https://www.agendaextranet.com.br/NovaExtranet/Inicial.aspx')
+   // cy.get('[window.openConfirmarCardapio.aspx','_blank').click()
   });
 });
